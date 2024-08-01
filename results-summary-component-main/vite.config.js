afd0ca8b965acb1a7d256649689969e531c54bae
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2024-08-01 10:51:14 +0900

    base 경로 수정

--- results-summary-component-main/vite.config.js
+++ results-summary-component-main/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/JS-frameworks-libraries-practice/results-summary-component-main/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
